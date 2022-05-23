51d21b17afb61a4c666094297d2d1f901f6935d2
minyoung529 <dlalsdud102613@gmail.com>
2022-05-23 14:54:39 +0900

    포탈 180도 막음

--- Assets/01.Scripts/ETC/Object/PortalObject.cs
+++ Assets/01.Scripts/ETC/Object/PortalObject.cs
@@ -6,11 +6,12 @@ public class PortalObject : MonoBehaviour
 {
     [SerializeField] private Transform linkedPortal;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
         if (linkedPortal == null) return;
+        if (Vector3.Dot(other.transform.position, transform.position) < 0f) return;
 
-        CharacterMovement character = collision.gameObject.GetComponent<CharacterMovement>();
+        CharacterMovement character = other.gameObject.GetComponent<CharacterMovement>();
 
         if(character)
         {
